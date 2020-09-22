2b667ed3d0b50bcaf627cfbef7a9a8e9b015dbc0
Sergio <sergioralandison@outlook.com>
2020-09-22 16:38:29 +0200

    :construction: work in progress

--- header.php
+++ header.php
@@ -3,6 +3,38 @@
   <head>
     <meta charset="utf-8"/>
     <title>Multipurpose Theme</title>
+    <link rel="stylesheet" href="assets/main.css">
   </head>
 
   <body>
+
+    <!-- main header -->
+
+    <header>
+      <nav>
+          <div class="">
+            <img src="" alt="">Lybra
+          </div>
+          <div class="">
+            <a href="#">DEMOS</a>
+            <a href="#">PAGES</a>
+            <a href="#">ELEMENTS</a>
+            <a href="#">BLOG</a>
+            <a href="#">CONTACTS</a>
+          </div>
+      </nav>
+
+      <div class="">
+        <p>MULTIPURPOSE</p>
+        <h1>Corporate <br></h1>
+        <p>
+          Will It be used for your home, your office or perhaps even <br>
+          your home office combo?
+        </p>
+      </div>
+
+      <form class="" action="index.html" method="post">
+        <input type="text" name="" value="" placeholder="+86 700">
+        <button type="button" name="contact">CONTACT ME</button>
+      </form>
+    </header>
