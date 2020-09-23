0108a321966aabba3069cc3f95cbebb3e2f8978f
Sergio <sergioralandison@outlook.com>
2020-09-23 20:28:54 +0200

    section and aside part added for footer

--- footer.php
+++ footer.php
@@ -1,4 +1,43 @@
+  <section>
+    <p>Subscribe to our newsletter</p>
+    <form class="" action="" method="post">
+      <input type="text" name="email" value="" placeholder="freebies@sales.com">
+      <button type="button" name="button">Subscribe</button>
+    </form>
 
+    <aside class="">
+      <h5>CONTACTS</h5>
+      <p>50 Raritan Center Pkwy, Edison, NJ 08837, USA</p>
+      <img src="images/icon-phone.png" alt="">
+      <p>8080000000</p>
+      <img src="images/icon-email.png" alt="">
+      <p>freebies@sales.com</p>
+      <form class="" action="" method="post">
+        <button type="button" name="contactUs">Contact Us</button>
+        <button type="button" name="visitUs">Visit Us</button>
+      </form>
+    </aside>
+
+    <aside class="">
+      <h5>Popular</h5>
+      <p>Free videos</p>
+      <p>Tools, apps and plugins</p>
+      <p>Blog</p>
+      <p>Photo license</p>
+      <p>FAQ</p>
+      <p>API</p>
+    </aside>
+
+    <aside class="">
+      <h5>Service</h5>
+      <p>Free videos</p>
+      <p>Tools, apps and plugins</p>
+      <p>Blog</p>
+      <p>Photo license</p>
+      <p>FAQ</p>
+      <p>API</p>
+    </aside>
+  </section>
 
   </body>
 </html>
